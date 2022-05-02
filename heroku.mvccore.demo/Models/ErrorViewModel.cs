4b95146158134dd64d068fdb60d8e2582c7281da
Trinh Duong <trinh.duong@tekcent.com>
2022-05-02 16:12:13 +0700

    Add project files.

---
+++ heroku.mvccore.demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace heroku.mvccore.demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
